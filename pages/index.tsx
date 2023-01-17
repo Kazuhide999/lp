acc34de0ffccdcf3739c79374f360201b7d49b63
Kazuhide999 <u.sqfx99@gmail.com>
2023-01-17 19:08:26 +0900

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,17 @@ const Home: NextPage = () => {
             <title>Tairaporomote</title>
         </Head>
         <div>
-            <video className='video'src='/test.mp4' autoPlay loop muted playsInline></video>
+            <video className='video'src='/tplp_title_bg.mp4' autoPlay loop muted playsInline></video>
             <div className="mv_txt">
-                <p>
-                    学{' '}び{' '}を、
+                {/*  */}
+                <p className='p-4'>
+                    <span className='p-2'>悩</span>{' '}<span className='p-2'>み</span>{' '}<span className='p-2'>を</span>{' '}<span className='p-2'>、</span>
                 </p>
-                <p>
-                    も{' '}っ{' '}と、
+                <p className='p-4'>
+                    <span className='p-2'>課</span>{' '}<span className='p-2'>題</span>{' '}<span className='p-2'>に</span>{' '}<span className='p-2'>、</span>
                 </p>
-                <p>
-                    新{' '}し{' '}く。
+                <p className='p-4'>
+                    <span className='p-2'>解</span>{' '}<span className='p-2'>決</span>{' '}<span className='p-2'>へ</span>{' '}<span className='p-2'>。</span>
                 </p>
             </div>
         </div>
@@ -33,7 +34,7 @@ const Home: NextPage = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-3 stroke-orange-700">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                     </svg>
-                    グループ内統一のマニュアルがなく、個々の施設毎に異なる手順、クオリティで業務が行われている…。
+                    工場に製造手順書や報告書などの帳票が多く管理が大変…。
                 </li>
                 <li className='mt-5 flex items-center space-x-3'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-3 stroke-orange-700">
@@ -51,13 +52,13 @@ const Home: NextPage = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-3 stroke-orange-700">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                     </svg>
-                    外国人就労者へのノウハウ伝達がうまくいかない…。
+                    紙ベースのマニュアルで、情報共有、マニュアル更新が非効率…。
                 </li>                
             </div>
         </div>
         <div className="cont2_title">
             <p>
-                Taira{' '}Promote{' '}なら
+                <span className='text-orange-600'>T</span>aira{' '}Promote{' '}<span className='text-3xl'>なら</span>
             </p>
         </div>
         <div className="containar2-2 py-6">      
@@ -65,7 +66,7 @@ const Home: NextPage = () => {
             <div className="accordion">
                 <details className="accordion_details">
                     <summary className="accordion_summary">
-                        グループ内統一のマニュアルがなく、個々の施設毎に異なる手順、クオリティで業務が行われている…。
+                        工場に製造手順書や報告書などの帳票が多く管理が大変…。
                     </summary>
                         {/* flow1 */}
                         <div className="flow_l details-content">
@@ -85,10 +86,10 @@ const Home: NextPage = () => {
                                     </p>    
                                     <div className="font-medium">
                                         <li>
-                                            課題グループ内での作業手順を作成
+                                            製造手順書や報告書などの帳票の管理を効率化
                                         </li>
                                         <li>
-                                            提供するサービスのクオリティの統一と標準化
+                                            製造手順書や報告書などの帳票作成のミスを低減
                                         </li>
                                     </div>
                                 </div>                               
@@ -112,8 +113,13 @@ const Home: NextPage = () => {
                                     </p>
                                     <div>
                                         <p className="font-medium">
-                                            パワーポイントを使った温かみのあるマニュアルの作成。
-                                            パワーポイントを使用しているので、納品後でもグループ内で更新が可能。
+                                            製造手順書や報告書などの帳票を1つのアプリケーションに集約し効率化する。
+                                            弊社のアプリケーション{' '}
+                                            <a href='https://www.tairapromote.co.jp/kupu/kupu.html'target="_blank" className='text-blue-700 underline underline-offset-2 mx-2 hover:bg-gray-200 rounded-md'>kupu</a>
+                                            {' '}をベースに翻訳機能を拡張することで開発コストを抑える。
+                                        </p>
+                                        <p className="font-medium">
+                                            作成ミスを低減するため、？？？堤さんにインタビュー？？？
                                         </p>
                                     </div>
                                 </div>    
@@ -139,7 +145,7 @@ const Home: NextPage = () => {
                                     </p>    
                                     <div className="font-medium">
                                         <p>
-                                            不明。仮屋さんにインタビューを行う。
+                                            不明。堤さんにインタビューを行う。
                                         </p>
                                     </div>
                                 </div>                               
@@ -159,21 +165,21 @@ const Home: NextPage = () => {
                                     </p>
                                     <div className='font-medium text-lg mt-2'>
                                         <p>
-                                            全施設共通のサービスを提供が可能になり、
+                                            1つアプリケーションに集約することによって、
                                         </p>
                                         <p className="text-red-500">
-                                            顧客満足度の向上
+                                            各帳票の管理・作成効率が向上
                                         </p>
                                         <p className="text-red-500">
-                                            売上拡大
+                                            各帳票の作成ミスが低減
                                         </p>
                                         <p className="text-red-500">
-                                            人材育成コスト削減
+                                            バラバラだった手順書のフォーマットが揃うことで、手順理解度が向上
                                         </p>                                        
                                     </div>
                                 </div>    
                             </div>
-                            <Image src='/human_img.png' alt="problem" width={270} height={270}/>
+                            <Image src='/step_icon_04.png' alt="problem" width={200} height={200}/>
                         </div>  
                 </details>
             </div>
@@ -293,7 +299,7 @@ const Home: NextPage = () => {
                                     </div>
                                 </div>    
                             </div>
-                            <Image src='/human_img.png' alt="problem" width={270} height={270}/>
+                            <Image src='/step_icon_04.png' alt="problem" width={200} height={200}/>
                         </div>    
                 </details>
             </div>
@@ -412,7 +418,7 @@ const Home: NextPage = () => {
                                     </div>
                                 </div>    
                             </div>
-                            <Image src='/human_img.png' alt="problem" width={270} height={270}/>
+                            <Image src='/step_icon_04.png' alt="problem" width={200} height={200}/>
                         </div> 
                 </details>
             </div>
@@ -420,7 +426,7 @@ const Home: NextPage = () => {
             <div className="accordion">
                 <details className="accordion_details">
                     <summary className="accordion_summary">
-                        外国人就労者へのノウハウ伝達がうまくいかない…。
+                        紙ベースのマニュアルで、情報共有、マニュアル更新が非効率…。
                     </summary>
                         {/* flow1 */}
                         <div className="flow_l details-content">
@@ -440,10 +446,10 @@ const Home: NextPage = () => {
                                     </p>    
                                     <div className="font-medium">
                                         <li>
-                                            手順などを日本語で記述して自動で現地語に変換
+                                            マニュアルにデジタル技術を活用することによる業務効率化
                                         </li>
                                         <li>
-                                            外国人就労者の理解度を向上する
+                                            誰でも簡単にマニュアルを更新できる
                                         </li>
                                     </div>
                                 </div>                               
@@ -467,8 +473,10 @@ const Home: NextPage = () => {
                                     </p>
                                     <div>
                                         <p className="font-medium">
-                                            弊社のアプリケーション{' '}<a href='https://www.tairapromote.co.jp/kupu/kupu.html'target="_blank" className='text-blue-700 underline underline-offset-2 mx-2 hover:bg-gray-200 rounded-md'>kupu</a>{' '}をベースに翻訳機能を拡張。
-                                            外国人就労者の考え方、感じ方を考慮したマニュアルを作成し解りやすさを向上させる。
+                                            Google Spredsheetを活用し、誰でも簡単に更新できるアプリケーションを制作する。
+                                        </p>
+                                        <p className="font-medium">
+                                            デザインは専用設計を行い使用ユーザーの理解度が向上することによって、提供サービスのクオリティを統一させ、顧客満足度を向上させる。
                                         </p>
                                     </div>
                                 </div>    
@@ -494,7 +502,7 @@ const Home: NextPage = () => {
                                     </p>    
                                     <div className="font-medium">
                                         <p>
-                                            不明。堤さんにインタビューを行う。
+                                            不明。徳田さん、仮屋さんにインタビューを行う。
                                         </p>
                                     </div>
                                 </div>                               
@@ -514,21 +522,24 @@ const Home: NextPage = () => {
                                     </p>
                                     <div className='font-medium text-lg mt-2'>
                                         <p>
-                                            現地語の資料と、外国人就労者の理解度が向上したことにより、
+                                            マニュアルにデジタル技術を活用することにより、
                                         </p>
                                         <p className="text-red-500">
-                                            人材育成体制の確保
+                                            更新展開のコストが30%削減
                                         </p>
                                         <p className="text-red-500">
-                                            人材育成コスト削減
+                                            人材育成の効率向上
                                         </p>
                                         <p className="text-red-500">
-                                            品質向上
+                                            提供サービスのクオリティ向上
+                                        </p>
+                                        <p className="text-red-500">
+                                            顧客満足度向上
                                         </p>                                        
                                     </div>
                                 </div>    
                             </div>
-                            <Image src='/human_img.png' alt="problem" width={270} height={270}/>
+                            <Image src='/step_icon_04.png' alt="problem" width={200} height={200}/>
                         </div>      
                 </details>
             </div>
@@ -555,22 +566,19 @@ const Home: NextPage = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-10 h-10 mr-3 stroke-red-500">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
                         </svg>
-                        弊社では1つの資料制作をベースに、カタログやWeb 媒体への展開につなげるコンテンツのエコフレームワークを推奨しています。
-                        そのためトータルでかかる広告販促費や媒体間で発生するコミュニケーションストレスを最小限にすることが可能です。
+                        弊社では1つのコンテンツ制作をベースに、カタログやWeb 媒体への展開につなげるため、トータルでかかる広告販促費や媒体間で発生するコミュニケーションストレスを最小限にすることが可能です。
                     </li>
                     <li className='mt-5 flex items-center space-x-3'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-10 h-10 mr-3 stroke-red-500">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
                         </svg>
-                        現地・現物で丁寧に取材・検討を行い、高品質で使用ユーザー満足度の高い資料を制作できます。
-                        ライティングから、イラストや動画、3Dモデルまで、すべて弊社で制作できるため、様々なご要望に柔軟に対応することができます。
+                        現地・現物で丁寧に取材・検討を行い、企業課題に合わせた様々なコンテンツを企画・提案・制作・運用まで、様々な要望に柔軟に対応できます。
                     </li>
                     <li className='mt-5 flex items-center space-x-3'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-10 h-10 mr-3 stroke-red-500">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
                         </svg>
-                        専門性の高いスタッフが多く在籍しているため、CAD図面の正確な読み取りを行えます。
-                        そのため、ご担当者様のご説明などのご負担を最小限にできると共に、高品質な資料を制作することができます。
+                        専門性の高いスタッフが多く在籍しており、ご担当者様のご負担を最小限にできると共に、高品質な資料を制作することができます。
                     </li>
                 </div>
                 <button type="button" className="btn2">
